6ad479f5e1c7ce45071b32ec6291b86af673e591
Daniel Galvão <galvaodev@gmail.com>
2017-02-27 16:35:36 -0300

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    entry: './app/App.js',
+    output: {
+        path: './public',
+        filename: 'bundle.js'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: [/node_modules/],
+          use: [{
+            loader: 'babel-loader',
+            options: { presets: ['es2015','react'] }
+          }],
+        },
+      ],
+    },
+}
